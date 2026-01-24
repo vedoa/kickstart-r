695d6d216f92b2dd90f876a661a2c0caa5bc1e60
vedoa <>
2026-01-24 12:36:11 +0100

    Fix tinytest problem and add sugest for the test backend.

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,5 @@
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  test_package("{{ package_name }}")
+  tinytest::test_package("{{ package_name }}")
 } else {
   warning("tinytest is not installed; tests not run.")
 }
