0affa26452e783244388bfef64297dbe5a9486bc
vedoa <>
2026-01-24 10:20:50 +0100

    From github yaml syntax to env in bash

--- kickstart-download.sh
+++ kickstart-download.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 VERSION="v0.6.0"
 
-if [[ "${{ runner.os }}" == "Linux" ]]; then
+if [[ "$RUNNER_OS" == "Linux" ]]; then
   FILE="kickstart-${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-elif [[ "${{ runner.os }}" == "macOS" ]]; then
+elif [[ "$RUNNER_OS" == "macOS" ]]; then
   FILE="kickstart-${VERSION}-x86_64-apple-darwin.tar.gz"
 else
   FILE="kickstart-${VERSION}-x86_64-pc-windows-msvc.zip"
@@ -13,17 +13,17 @@ fi
 
 URL="https://github.com/Keats/kickstart/releases/download/${VERSION}/${FILE}"
 
-if [[ "${{ runner.os }}" == "Windows" ]]; then
+if [[ "$RUNNER_OS" == "Windows" ]]; then
   curl -L "$URL" -o kickstart_archive.zip
 else
   curl -L "$URL" -o kickstart_archive
 fi
 
-if [[ "${{ runner.os }}" == "Windows" ]]; then
+if [[ "$RUNNER_OS" == "Windows" ]]; then
   powershell -Command "Expand-Archive -Path kickstart_archive.zip -DestinationPath ."
-  echo "${{ github.workspace }}" >> $GITHUB_PATH
+  echo "$GITHUB_WORKSPACE" >> $GITHUB_PATH
 else
   tar -xzf kickstart_archive
   chmod +x kickstart
-  echo "${{ github.workspace }}" >> $GITHUB_PATH
+  echo "$GITHUB_WORKSPACE" >> $GITHUB_PATH
 fi
